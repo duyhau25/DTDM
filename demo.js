959d0969c3908dd9ab30f4ea9508a9b5b35f1a4d
qhuy2002 <150362793+qhuy2002@users.noreply.github.com>
2023-11-26 20:12:30 +0700

    Update demo.js

--- demo.js
+++ demo.js
@@ -5,3 +5,41 @@ console.log("Hello mn");
 huy
 hello huy đã ở đây
 kiên đã ở đây
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Huy Peos</title>
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
+      integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="card">
+      <div class="card__img">
+        <img src="https://i.imgur.com/FMexFHi.png" alt="" />
+      </div>
+      <h2>Nguyễn Quang Huy</h2>
+      <p>Sinh viên CNTT</p>
+      <div class="card__social">
+        <a target="_black" href="https://www.facebook.com/huypeos">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+        <a target="_black" href="https://github.com/qhuy2002"
+          href="">
+          <i class="fab fa-github"></i>
+        </a>
+      </div>
+      <button>Contact me</button>
+    </div>
+  </body>
+</html>
